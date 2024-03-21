a4004d1a8192dbadbe984f38cb2914f03a5219d4
KYLN24 <1296845690@qq.com>
2024-03-21 17:26:19 +0800

    close [Feature Request] 添加旧站点路径的重定向 #2

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,6 +1,37 @@
 import './main.css'
-import './tailwind.css'
-
 import DefaultTheme from 'vitepress/theme'
 
-export default { ...DefaultTheme }
+let customRoutes = [{
+    "path": "/version",
+    "redirect": "https://github.com/DanXi-Dev/DanXi/releases"
+}, {
+    "path": "/privacy",
+    "redirect": "/doc/app-privacy"
+}, {
+    "path": "/terms-and-condition",
+    "redirect": "/doc/app-terms-and-condition"
+}, {
+    "path": "/support",
+    "redirect": "https://danxi-dev.feishu.cn/wiki/wikcnrPPGDCiTODBYRkdwLlHH65"
+}, {
+    "path": "/about",
+    "redirect": "/"
+}]
+
+export default {
+    ...DefaultTheme,
+    enhanceApp({ app, router, siteData }) {
+        router.onBeforeRouteChange = async (to) => {
+            for (let route of customRoutes) {
+                if (route.path == to) {
+                    if (route.redirect.startsWith("http")) {
+                        window.location.href = route.redirect
+                    } else {
+                        await router.go(route.redirect)
+                    }
+                    return false
+                }
+            }
+        }
+    }
+}
