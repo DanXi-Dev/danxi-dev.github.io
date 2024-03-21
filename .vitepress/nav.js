62142bdf2873c16ef665eca3d15e7ac8c3a96ea6
KYLN24 <1296845690@qq.com>
2024-03-21 19:15:26 +0800

    update friend link

--- .vitepress/nav.js
+++ .vitepress/nav.js
@@ -13,10 +13,12 @@ export default [
     // },
     {
         text: '友情链接', items: [
+            // { text: '复旦大学', link: 'https://www.fudan.edu.cn' },
+            // { text: '复旦大学计算机科学技术学院', link: 'https://cs.fudan.edu.cn' },
             { text: 'DanXi-Dev', link: 'https://github.com/DanXi-Dev' },
             { text: 'OpenTreeHole', link: 'https://github.com/OpenTreeHole' },
             { text: '行云小站', link: 'https://w568w.eu.org/' },
-            { text: 'FDCraft | 复读世界𒆙', link: 'https://fdcraft.top' }
+            { text: 'FDCraft | 复读世界𒆙', link: 'https://fdcraft.github.io/' }
         ]
     }
 ]
