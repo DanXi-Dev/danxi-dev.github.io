5b7016096da656afb41e5687b0671d924949812f
Zhang Wenbo <100940438+ppolariss@users.noreply.github.com>
2025-02-26 21:02:46 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss'
 export default {
   content: [
     "./{.vitepress,src}/**/*.{html,js,ts,vue,md}",
+    "./src/public/.well-known/apple-app-site-association"
   ],
   theme: {
     extend: {},
