a988f1f1950d1249b957e55bfed1339ead4ef21d
shdancer <lumos@lumos.fit>
2025-04-14 22:09:40 +0800

    feat: finish celebration

--- src/components/BlessingBar.vue
+++ src/components/BlessingBar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from 'vue';
+import { computed, ref } from 'vue';
 import { useFetch } from '@vueuse/core';
 
 const floorNumber = 114514
@@ -10,39 +10,47 @@ const positionString = computed(() => {
     return `${position.value}%`
 })
 
+const stop = ref(false)
 
-const { isFetching, error, data } = useFetch(`/api/floors/${floorNumber}/_special?order_by=id&size=100&sort=asc&offset=0`).get().json()
+
+const { isFetching, error, data } = useFetch(`/api/floors/${floorNumber}/_special?order_by=like&size=100&sort=asc&offset=0`).get().json()
 
 const messages: any = computed(() =>
     data.value?.map(item => ({
         content: item.content,
         name: item.anonyname,
-    }))?.filter(item => !item.content.includes("!()")) ?? []
+    }))?.filter(item => !item.content.includes("![]")) ?? []
 )
 const interval = setInterval(() => {
+    if (stop.value) {
+        return
+    }
     position.value -= 0.003
     if (position.value > 100) {
         position.value = 0
     }
 }, 10)
 
+function toHole() {
+    window.location.href = "/celebration/hole"
+}
 
 
 </script>
 
 <template>
     <div class="bg-gray-100 rounded-xl shadow-inner overflow-hidden p-3 flex ">
-        <div class="flex items-center gap-4 flex-nowrap move">
-            <div v-for="message in messages">
-                <div class="flex text-nowrap">
-                    <b>
-                        {{ message.content }}
-                    </b>
-                    <i>
-                        by
-                        {{ message.name }}
-                    </i>
-                </div>
+        <div class="flex items-center gap-4 flex-nowrap move" @mouseenter="stop = true" @mouseleave="stop = false"
+            @click="toHole">
+            <div v-for="message in messages"
+                class="flex text-nowrap pl-2 pr-2 p-1 shadow-md rounded-md hover:shadow-lg transition-all duration-300 ease-in-out hover:bg-gray-200 cursor-pointer">
+                <b class="max-w-64 text-ellipsis overflow-hidden mr-2 inline-block">
+                    {{ message.content }}
+                </b>
+                <i>
+                    by
+                    {{ message.name }}
+                </i>
             </div>
         </div>
 
---
+++ src/views/GalleryView.vue
@@ -0,0 +1,112 @@
+<script setup>
+function goBack() {
+    window.history.back()
+}
+</script>
+
+<template>
+    <section class="md:w-2/3 m-auto w-full pl-10 pr-10 mt-2 flex flex-col gap-3">
+        <el-page-header @back="goBack">
+            <template #content>
+                <span class="text-large font-600 mr-3"> 赛博美术馆 </span>
+            </template>
+            <template #title>
+                <span class="text-large font-600 mr-3"> 返回 </span>
+            </template>
+        </el-page-header>
+        <h1 class="text-2xl font-bold">旦挞四周年文创·赛博美术馆——就在此处！</h1>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年吧唧·四款
+            </h2>
+            <div class="flex gap-1">
+                <img src="/assets/gallery/paji-1.webp" alt="" class="w-1/4">
+                <img src="/assets/gallery/paji-2.webp" alt="" class="w-1/4">
+                <img src="/assets/gallery/paji-3.webp" alt="" class="w-1/4">
+                <img src="/assets/gallery/paji-4.webp" alt="" class="w-1/4">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年表情包贴纸·五组
+            </h2>
+            <div class="flex gap-1">
+                <img src="/assets/gallery/meme-1.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/meme-2.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/meme-3.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/meme-4.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/meme-5.webp" alt="" class="w-1/5">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年钥匙扣·五个
+            </h2>
+            <div class="flex gap-1">
+                <img src="/assets/gallery/keyring-1.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/keyring-2.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/keyring-3.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/keyring-4.webp" alt="" class="w-1/5">
+                <img src="/assets/gallery/keyring-5.webp" alt="" class="w-1/5">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年光栅卡·六张
+            </h2>
+            <div class="flex gap-1 justify-center">
+                <img src="/assets/gallery/card-1.webp" alt="" class="w-1/6">
+                <img src="/assets/gallery/card-2.webp" alt="" class="w-1/6">
+                <img src="/assets/gallery/card-3.webp" alt="" class="w-1/6">
+                <img src="/assets/gallery/card-4.webp" alt="" class="w-1/6">
+                <img src="/assets/gallery/card-5.webp" alt="" class="w-1/6">
+                <img src="/assets/gallery/card-6.webp" alt="" class="w-1/6">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年立绘
+            </h2>
+            <div class="flex gap-1 justify-center">
+                <img src="/assets/gallery/painting.webp" alt="" class="w-1/3">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年纸袋
+            </h2>
+            <div class="flex gap-1 justify-center">
+                <img src="/assets/gallery/bag.webp" alt="" class="w-1/2">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年海报
+            </h2>
+            <div class="flex gap-1 justify-center">
+                <img src="/assets/gallery/post.webp" alt="" class="w-1/2">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年横幅
+            </h2>
+            <div class="flex flex-col gap-1 justify-center">
+                <img src="/assets/gallery/banner2.webp" alt="" class="w-full">
+                <img src="/assets/gallery/banner1.webp" alt="" class="w-full">
+            </div>
+        </section>
+        <section class="w-full rounded-3xl border-2 border-blue-200 p-5 ">
+            <h2 class="text-xl font-semibold">
+                四周年头图
+            </h2>
+            <div class="flex flex-col gap-1 justify-center">
+                <img src="/assets/gallery/banner3.webp" alt="" class="w-full">
+            </div>
+        </section>
+
+        <p class=" text-gray-500 text-sm font-thin mb-2">旦挞四周年文创美工制作：@MSapphire（伍芷萱） @masa（赵佳凝）
+            @vickyvia114（张文惠）
+        </p>
+    </section>
+</template>
